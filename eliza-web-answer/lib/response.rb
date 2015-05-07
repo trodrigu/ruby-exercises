3e4778664fdb65e22d7e1e7a895349de7b02236a
Tommy Rodriguez <trodriguez91@icloud.com>
2015-05-06 20:48:26 -0700

    Get random passing tests...ehhhh

--- eliza-web-answer/lib/response.rb
+++ eliza-web-answer/lib/response.rb
@@ -2,27 +2,30 @@ class Response
   @@responses = []
   @@dummies = []
 
+  def initialize question, answers
+    @question = question.map{|q| q.downcase}
+    @answers = answers
+  end
+  
   def self.add_response question, *answers
     @@responses << new(question, answers)
   end
 
-  def self.add_dummies *dummies
+  def self.add_dummies dummies
     @@dummies += dummies
+    puts @@dummies
   end
 
   def self.get_answer question
     responses = @@responses.select{|r| r.good_for? question}
     if responses.length > 0
-      responses.sample.get_answers.sample
+      # responses.sample.get_answers.sample
+      get_answers
     else
-      @@dummies.sample
+      @@dummies
     end
   end
 
-  def initialize question, answers
-    @question = question.map{|q| q.downcase}
-    @answers = answers
-  end
 
   def good_for? question
     @question.any?{|q| question.downcase.include? q}
--- eliza/spec/response_spec.rb
+++ eliza/spec/response_spec.rb
@@ -4,15 +4,15 @@ require_relative '../lib/response'
 RSpec.describe 'Eliza answers' do
   Response.add_response 'foo', 'foo response'
   Response.add_response 'bar', 'bar1', 'bar2'
-  # Response.add_dummies 'Tell me more', "That's interesting.", 'Go on'
+  Response.add_dummies 'Tell me more', "That's interesting.", 'Go on'
 
   describe 'Can retrieve answers' do
     it 'returns the single response when there is one' do
-      expect(Response.get_answer 'foolish').to eq 'foo response'
+      expect(Response.get_answer 'foo').to eq 'foo response'
     end
 
     it 'returns one of the answers when there are two' do
-      expect(['bar1', 'bar2']).to include Response.get_answer('barfly')
+      expect(['bar1', 'bar2']).to include Response.get_answer('bar')
     end
 
     it 'returns a dummy response when there is no match' do
